b458abbefa00ad3f4d588c736101ac971e84ced2
rizemmahendra <ryzemmahendra@gmail.com>
2024-12-02 16:00:41 +0700

    feat(wifi): create WifiService class to handle wifi connection

---
+++ lib/WifiService/WifiService.h
@@ -0,0 +1,24 @@
+#ifndef Wifi_Service_H
+#define Wifi_service_H
+
+#include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+
+class WifiService
+{
+private:
+    String _ssid;
+    String _password;
+    String _autoReconnect;
+
+    friend void WiFiGotIP(WiFiEvent_t event, WiFiEventInfo_t info);
+    friend void WiFiConnected(WiFiEvent_t event, WiFiEventInfo_t info);
+
+public:
+    WifiService(const char *ssid, const char *password, bool autoReconnect = true);
+    void connect();
+    bool ready();
+};
+
+#endif
